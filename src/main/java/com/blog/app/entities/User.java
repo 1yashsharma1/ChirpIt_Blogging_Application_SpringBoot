4a72f923678f521cb94bd5c5d410e8cb6a2c9b18
1yashsharma1 <syash832@gmail.com>
2023-11-04 18:39:12 +0530

    updating project settings

---
+++ src/main/java/com/blog/app/entities/User.java
@@ -0,0 +1,36 @@
+package com.blog.app.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "user_name", nullable = false, length = 100)
+	private String name;
+
+	@Column(nullable = false)
+	private String email;
+
+	@Column(length = 50, nullable = false)
+	private String password;
+
+	@Column(length = 200)
+	private String about;
+
+}
