e065d76f54d32ff8acc354f2f0699147165e7cf0
1yashsharma1 <syash832@gmail.com>
2023-11-06 21:19:45 +0530

    Adding Post java class.
    Adding mapping of post to Category and User java class

--- src/main/java/com/blog/app/entities/Category.java
+++ src/main/java/com/blog/app/entities/Category.java
@@ -1,10 +1,15 @@
 package com.blog.app.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -27,4 +32,6 @@ public class Category {
 	@Column(name = "description", length = 200, nullable = false)
 	private String categoryDescription;
 
+	@OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+	private List<Post> posts = new ArrayList<>();
 }
