5040131760226ca45e83bfd91bfcbcf4e608c385
1yashsharma1 <syash832@gmail.com>
2023-11-14 23:23:32 +0530

    Adding comment entities and DTOs and mapping with post class and DTOs

--- src/main/java/com/blog/app/entities/Post.java
+++ src/main/java/com/blog/app/entities/Post.java
@@ -1,14 +1,18 @@
 package com.blog.app.entities;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -43,4 +47,7 @@ public class Post {
 
 	@ManyToOne
 	private User user;
+
+	@OneToMany(mappedBy = "post", fetch = FetchType.EAGER)
+	private List<Comment> comments = new ArrayList<>();
 }
